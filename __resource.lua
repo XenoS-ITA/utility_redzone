9548d45048469bdfc0a039e8850c8e585fe00b02
Xenos.exe <55803068+XenoS-ITA@users.noreply.github.com>
2021-07-03 21:08:42 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,11 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+
+game 'gta5'
 
 client_scripts {
     "@utility_lib/client/native.lua",
     "config.lua",
     "client.lua",
 }
+
+dependency 'utility_lib'
